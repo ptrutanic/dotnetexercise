d265cb9f170731269ed8ec438197e09c5a354658
Petar Trutanic <petar.trutanic@gmail.com>
2025-07-29 17:35:30 +0200

    Fixed typo

--- AbySalto.Mid.Infrastructure/DependencyInjection.cs
+++ AbySalto.Mid.Infrastructure/DependencyInjection.cs
@@ -5,7 +5,7 @@ namespace AbySalto.Mid.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructur(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
             return services;
         }
