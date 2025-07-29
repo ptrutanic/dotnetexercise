47f82dae3017a8530e35042d45ce5153ddd585c8
Petar Trutanic <petar.trutanic@gmail.com>
2025-07-29 17:35:41 +0200

    Added dummy controller

--- AbySalto.Mid/Controllers/BasketController.cs
+++ AbySalto.Mid/Controllers/BasketController.cs
@@ -2,7 +2,20 @@
 
 namespace AbySalto.Mid.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class BasketController : Controller
     {
+        [HttpGet]
+        public IActionResult GetBaskets()
+        {
+            var baskets = new List<object>
+            {
+                new { Id = 1, Name = "Fruit Basket", Quantity = 5 },
+                new { Id = 2, Name = "Vegetable Basket", Quantity = 3 }
+            };
+
+            return Ok(baskets);
+        }
     }
 }
